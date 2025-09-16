ea4ed58422a81268f5b73111ff9e6604c74a9443
ahlaiptek <iptekahla@gmail.com>
2025-09-17 04:13:31 +0700

    Add Laporan model with mass assignable attributes and soft deletes

---
+++ app/Models/Laporan.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Enums\LaporanStatus;
+use App\Enums\LaporanPrioritas;
+
+class Laporan extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * Nama tabel yang terkait dengan model ini.
+     *
+     * @var string
+     */
+    protected $table = 'laporan';
+
+    /**
+     * Atribut yang dapat diisi secara massal (mass assignable).
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'judul_laporan',
+        'deskripsi',
+        'lokasi_detail',
+        'desa_id',
+        'status',
+        'prioritas',
+        'catatan_admin'
+    ];
+
+    /**
+     * Atribut yang harus diubah ke tipe data tertentu.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'status' => LaporanStatus::class,
+        'prioritas' => LaporanPrioritas::class,
+    ];
+}
