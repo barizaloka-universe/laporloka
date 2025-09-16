47a46817ba8b9a1f85d874bb08bb5985bc04cfd6
ahlaiptek <iptekahla@gmail.com>
2025-09-17 05:35:41 +0700

    Add permission configuration and migration for roles and permissions

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
